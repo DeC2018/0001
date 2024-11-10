b6bb36a52a6ff192cdfdae85cf8a3f24a4b5f88e
DeC2018 <dec2018@noreply.gitverse.ru>
2024-11-10 03:43:50 +0000

    create main.cpp

---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> lookup;
+        for (int i = 0; i < nums.size(); ++i) {
+            if (lookup.count(target - nums[i])) {
+                return {lookup[target - nums[i]], i};
+            }
+            lookup[nums[i]] = i;
+        }
+        return {};
+    }
+};
+
+int main() {
+    vector<int> nums = {2, 7, 11, 15};
+    int target = 9;
+    Solution solution;
+    vector<int> result = solution.twoSum(nums, target);
+
+    if (!result.empty()) {
+        cout << "Indices: " << result[0] << ", " << result[1] << endl;
+    } else {
+        cout << "No solution found." << endl;
+    }
+
+    return 0;
+}
